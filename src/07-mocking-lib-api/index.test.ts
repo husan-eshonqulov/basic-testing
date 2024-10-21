33b961f7e8204ed6816082c81144702b0e1d57ef
Husan Eshonqulov <husan.eshonqulov.00@gmail.com>
2024-10-22 00:36:18 +0500

    mocking-lib-api done

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,62 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi, THROTTLE_TIME } from './index';
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const relativePath = 'users';
+
+    const axiosInstance: Partial<AxiosInstance> = {
+      get: jest.fn().mockResolvedValue({ data: { id: 1, name: 'John Doe' } }),
+    };
+    const createSpy = jest.spyOn(axios, 'create');
+
+    createSpy.mockReturnValue(axiosInstance as AxiosInstance);
+    await throttledGetDataFromApi(relativePath);
+
+    expect(createSpy).toBeCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
+
+    createSpy.mockRestore();
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const relativePath = 'users';
+
+    const axiosInstance: Partial<AxiosInstance> = {
+      get: jest.fn().mockReturnValue({ data: { id: 1, name: 'John Doe' } }),
+    };
+    const createSpy = jest.spyOn(axios, 'create');
+
+    createSpy.mockReturnValue(axiosInstance as AxiosInstance);
+    jest.useFakeTimers();
+    jest.advanceTimersByTime(THROTTLE_TIME);
+    await throttledGetDataFromApi(relativePath);
+
+    expect(axiosInstance.get).toBeCalledWith('users');
+
+    jest.useRealTimers();
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const relativePath = 'users';
+
+    const axiosInstance: Partial<AxiosInstance> = {
+      get: jest
+        .fn()
+        .mockResolvedValueOnce({ data: { id: 1, name: 'John Doe' } }),
+    };
+    const createSpy = jest.spyOn(axios, 'create');
+
+    createSpy.mockReturnValue(axiosInstance as AxiosInstance);
+    jest.useFakeTimers();
+    jest.advanceTimersByTime(THROTTLE_TIME);
+
+    await expect(throttledGetDataFromApi(relativePath)).resolves.toEqual({
+      id: 1,
+      name: 'John Doe',
+    });
+
+    jest.useRealTimers();
   });
 });
